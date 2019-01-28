a9ad1c1ffef11cd6b672d2667e6e9c5e6bfa4ce1
rezpo <danrebo@gmail.com>
2019-01-28 08:23:09 -0300

    Updates

---
+++ precache-manifest.666a4f977788fae944e056a52d265451.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f596ebd673d67838a7e348f668340863",
+    "url": "/template-killhumans/static/media/robot-brillo.f596ebd6.svg"
+  },
+  {
+    "revision": "57e6442b06a4416f9079",
+    "url": "/template-killhumans/static/css/main.e8b01dc7.chunk.css"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/template-killhumans/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "350b605e2752bd21aea8",
+    "url": "/template-killhumans/static/js/1.350b605e.chunk.js"
+  },
+  {
+    "revision": "7e4a49b67aac3dbf5331",
+    "url": "/template-killhumans/static/js/runtime~main.7e4a49b6.js"
+  },
+  {
+    "revision": "5aae3141c14339d81724bda20af849d2",
+    "url": "/template-killhumans/static/media/robot-torso.5aae3141.svg"
+  },
+  {
+    "revision": "2bd1fbfc6312368a23a7be784e209ae6",
+    "url": "/template-killhumans/static/media/robot-brazo-derecho.2bd1fbfc.svg"
+  },
+  {
+    "revision": "c4e7b30373f38eaabeca6fa2193907b1",
+    "url": "/template-killhumans/static/media/robot-brazo-izquierdo.c4e7b303.svg"
+  },
+  {
+    "revision": "57e6442b06a4416f9079",
+    "url": "/template-killhumans/static/js/main.57e6442b.chunk.js"
+  },
+  {
+    "revision": "43a47b6a3423975deeee1321a3dfc902",
+    "url": "/template-killhumans/static/media/robot-flot.43a47b6a.svg"
+  },
+  {
+    "revision": "1fc796067dfcb45c97a83c0fb494fff7",
+    "url": "/template-killhumans/static/media/robot-laser.1fc79606.svg"
+  },
+  {
+    "revision": "578c47138b0b3df72bb19dc5159de206",
+    "url": "/template-killhumans/static/media/robot-ojo.578c4713.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/template-killhumans/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/template-killhumans/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/template-killhumans/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "350b605e2752bd21aea8",
+    "url": "/template-killhumans/static/css/1.98f677c0.chunk.css"
+  },
+  {
+    "revision": "bab3edf5a371101eca337a14c49f516b",
+    "url": "/template-killhumans/index.html"
+  }
+];
